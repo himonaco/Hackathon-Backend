01e81f01d05269156c8fcf279afe3a0da14932f7
Your name <Your email>
2024-01-30 12:32:05 +0100

    ok

---
+++ models/trips.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+
+const tripsSchema = mongoose.Schema({
+    departure: String,
+    arrival: String,
+    date: String,
+    price: Number,
+})
+
+const Trip = mongoose.model('trips', tripsSchema)
+
+module.exports = Trip
