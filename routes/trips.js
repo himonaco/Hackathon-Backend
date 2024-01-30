060a68f8107399618af8e666a4642b4279a45ec0
Your name <Your email>
2024-01-30 12:55:42 +0100

    commit

--- routes/trips.js
+++ routes/trips.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+router.get('/:departure/:arrival', async (req, res) => {
+  try {
+    const { departure, arrival } = req.params;
+    const trips = await Trip.find({ departure, arrival });
+    res.json(trips);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 module.exports = router;
